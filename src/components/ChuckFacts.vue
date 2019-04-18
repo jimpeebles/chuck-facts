35a6a2ff4588907a614117ef7e7869f77f52c075
jimpeebles <james@concepttoproduct.us>
2019-04-18 18:41:26 -0400

    Get Chuck up and running

---
+++ src/components/ChuckFacts.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <div class="quote" @click="getQuote">
+      <h2>{{ quote }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: "CLICK FOR A CHUCK FACT"
+    };
+  },
+  computed: {},
+  methods: {
+    getQuote() {
+      this.quote = "( getting fact . . . )";
+      let self = this;
+      fetch("https://api.chucknorris.io/jokes/random")
+        .then(function(response) {
+          if (response.status !== 200) {
+            return;
+          }
+
+          // Examine the text in the response
+          response.json().then(function(res) {
+            self.quote = res.value;
+          });
+        })
+        .catch(function(err) {});
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  font-family: "Quicksand", Helvetica, sans-serif;
+  font-weight: 500;
+  font-size: 4em;
+  line-height: 120%;
+}
+.container {
+  text-align: center;
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  background-image: url("~/karate.png");
+  background-position: 0px 0px;
+  background-repeat: repeat-x repeat-y;
+
+  animation: animatedBackground 40s linear infinite;
+}
+.quote {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  width: 90%;
+  margin: 0 auto;
+  cursor: pointer;
+  &:hover {
+    color: red;
+  }
+  overflow-y: scroll;
+}
+
+@keyframes animatedBackground {
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: 0 100%;
+  }
+}
+</style>
